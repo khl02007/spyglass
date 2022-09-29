4484e0a5162f3448df98a295f0078e89ce69a097
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2022-09-28 17:05:14 -0700

    Update documentation action

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copy_tree("../notebooks", "./_copied_over/notebooks")
 
 project = 'Spyglass'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.1'
 author = 'Loren Frank'
 copyright = '2020 - Present, Loren Frank'
 
